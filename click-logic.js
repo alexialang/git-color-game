2cfb0bb4d01a98db7685ae670e5c8e1193c72d1d
NicolasCoquatrix <c.nicolas.perso@gmail.com>
2024-12-03 15:06:50 +0100

    feature:created the setupColorGame function that retrieves the color divs and the target

---
+++ click-logic.js
@@ -0,0 +1,6 @@
+function setupColorGame() {
+    const colorDivs = document.querySelectorAll('.color-div');
+    const targetDiv = document.querySelector('#target-color');
+
+    const targetColor = targetDiv.style.backgroundColor;
+}
