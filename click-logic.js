da7d24ff22685ba7af9528292db7e7530761ec36
NicolasCoquatrix <c.nicolas.perso@gmail.com>
2024-12-03 15:10:19 +0100

    feature : creation of the checkColor function which checks if the clicked color is the correct one

--- click-logic.js
+++ click-logic.js
@@ -4,3 +4,13 @@ function setupColorGame() {
 
     const targetColor = targetDiv.style.backgroundColor;
 }
+
+function checkColor(clickedDiv, targetColor) {
+    const clickedColor = clickedDiv.style.backgroundColor;
+
+    if (clickedColor === targetColor) {
+        alert('Bravo ! Vous avez cliqué sur la bonne couleur.');
+    } else {
+        alert('Dommage ! Ce n’est pas la bonne couleur.');
+    }
+}
