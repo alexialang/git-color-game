48c458d8e32d9c128a8b3c9fe0c68acac8fe1a2c
NicolasCoquatrix <c.nicolas.perso@gmail.com>
2024-12-03 16:54:25 +0100

    fix : bug fixes, but it doesn't work, but we don't have time anymore

--- logic.js
+++ logic.js
@@ -1,3 +1,30 @@
+const colorDivs = document.querySelectorAll('.color-div');
+const targetDiv = document.querySelector('#target-color');
+
+const targetColor = targetDiv.style.backgroundColor;
+
+colorDivs.forEach(div => {
+    div.addEventListener('click', function() {
+        checkColor(this, targetColor);
+    });
+});
+
+function checkColor(clickedDiv, targetColor) {
+    let scoreElement = document.querySelector('#score');
+    let score = parseInt(scoreElement.textContent);
+
+    const clickedColor = clickedDiv.style.backgroundColor;
+
+    console.log(clickedColor)
+    console.log(targetColor)
+    if (clickedColor === targetColor) {
+        score++;
+        scoreElement.textContent = score;
+        colorGeneration()
+        alert("OUII")
+    }
+}
+
 const colors = [
     '#FFB3BA', '#FFDFBA', '#FFFFBA', '#BAFFC9', '#BAE1FF',
     '#D5AAFF', '#FFC8DD', '#BDE0FE', '#A2D2FF', '#C3FBD8',
@@ -5,7 +32,7 @@ const colors = [
     '#FBE7C6', '#F5F0BB', '#D0F4DE', '#A9DEF9', '#E4C1F9'
 ];
 
-export function colorGeneration() {
+function colorGeneration() {
     const availableColors = [...colors];
 
     const targetColor = availableColors.splice(
@@ -15,6 +42,7 @@ export function colorGeneration() {
 
     const targetColorDiv = document.getElementById('target-color');
     targetColorDiv.style.backgroundColor = targetColor;
+    targetColorDiv.classList.add(targetColor);
 
     const colorDivs = document.querySelectorAll('.color-div');
 
@@ -23,6 +51,7 @@ export function colorGeneration() {
         const color = availableColors.splice(randomIndex, 1)[0];
         
         div.style.backgroundColor = color;
+        div.id = color
     });
 
     const randomDiv = colorDivs[Math.floor(Math.random() * colorDivs.length)];
